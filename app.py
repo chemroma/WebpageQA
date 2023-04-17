15e9101fae5d7321a5fbbab0e662a142267174bf
chemroma <130645584+chemroma@users.noreply.github.com>
2023-04-17 11:25:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ if "past" not in st.session_state:
 
 
 def get_text():
-    input_text = st.text_input("You: ", "Hello, how are you?", key="input")
+    input_text = st.text_input("Enter your url: ", key="input")
     return input_text
 
 
