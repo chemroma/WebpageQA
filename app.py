d9772ea8eceef949eafd5bfc211d0ef12a1267c6
chemroma <130645584+chemroma@users.noreply.github.com>
2023-04-17 12:15:45 +0800

    update app.py

--- app.py
+++ app.py
@@ -22,21 +22,20 @@ def get_text():
     input_text = st.text_input("Enter your url: ", key="input")
     return input_text
 
-while True:
-    user_input = get_text()
+user_input = get_text()
 
-    if user_input:
-        # result = chain({"question": user_input})
-        output = user_input
+if user_input:
+    # result = chain({"question": user_input})
+    output = user_input
 
-        #st.session_state.past.append(user_input)
-        #st.session_state.generated.append(output)
+    #st.session_state.past.append(user_input)
+    #st.session_state.generated.append(output)
 
-    message(user_input, is_user=True)  # align's the message to the right
-    message(user_input)
+# message(user_input, is_user=True)  # align's the message to the right
+# message(user_input)
     
-#if st.session_state["generated"]:
+if st.session_state["generated"]:
 
-#    for i in range(len(st.session_state["generated"]) - 1, -1, -1):
-#        message(st.session_state["generated"][i], key=str(i))
-#        message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
+   for i in range(len(st.session_state["generated"]) - 1, -1, -1):
+       message(st.session_state["generated"][i], key=str(i))
+       message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
