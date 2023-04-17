100da4de59def88f67f28d050b1cce45e5e3c202
chemroma <130645584+chemroma@users.noreply.github.com>
2023-04-17 16:38:35 +0800

    update

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ from langchain.chains import VectorDBQAWithSourcesChain
 import pickle
 from urllib.parse import urlparse
 
+from langchain.document_loaders import UnstructuredURLLoader, TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.embeddings import OpenAIEmbeddings
+
 def uri_validator(x):
     try:
         result = urlparse(x)
@@ -24,40 +29,39 @@ if "generated" not in st.session_state:
 if "past" not in st.session_state:
     st.session_state["past"] = []
 
+def get_api_key():
+    api_key = st.text_input("Enter your OpenAI API key: ", key="apikey")
+    return api_key
+
 def get_urls():
     urls = st.text_input("Enter your url: ", key="url")
     return urls
 
-
-def get_text():
+def get_query():
     input_text = st.text_input("Enter your OpenAI API key: ", key="input")
     return input_text
 
+api_key = api_key()
 urls = get_urls()
-user_input = get_text()
+query = get_query()
 
-placeholder = st.empty()
 input_ok = True
 if not uri_validator(urls.strip()):
     input_ok = False
 
 if not input_ok:
-    placeholder.error('please input a valid url.')
-
-st.info(f'DEBUG: url is {input_ok}')
+    st.error('please input a valid url.')
 
 if input_ok:
-    st.info('url is ok')
-    placeholder.empty()
-    if user_input:
-        # result = chain({"question": user_input})
-        output = user_input
+    if query:
+        # result = chain({"question": query})
+        output = query
 
-        st.session_state.past.append(user_input)
+        st.session_state.past.append(query)
         st.session_state.generated.append(output)
 
-    # message(user_input, is_user=True)  # align's the message to the right
-    # message(user_input)
+    # message(query, is_user=True)  # align's the message to the right
+    # message(query)
         
     if st.session_state["generated"]:
         for i in range(len(st.session_state["generated"]) - 1, -1, -1):
